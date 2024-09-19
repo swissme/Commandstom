899c6713bbd0b4ead0555ca27bbf0c9b5c60fc78
CDX <80155611+CDX-1@users.noreply.github.com>
2024-09-19 17:13:46 -0400

    Added basic command registering

--- cloudstom/src/main/java/net/minesprawl/cloudstom/MinestomCommandManager.java
+++ cloudstom/src/main/java/net/minesprawl/cloudstom/MinestomCommandManager.java
@@ -6,15 +6,37 @@ import org.incendo.cloud.CommandManager;
 import org.incendo.cloud.SenderMapper;
 import org.incendo.cloud.SenderMapperHolder;
 import org.incendo.cloud.execution.ExecutionCoordinator;
+import org.jetbrains.annotations.NotNull;
 
 public class MinestomCommandManager<C> extends CommandManager<C> implements SenderMapperHolder<CommandSender, C> {
     private final SenderMapper<CommandSender, C> senderMapper;
 
-    protected MinestomCommandManager(
+    // Only works if using normal CommandSender
+    public MinestomCommandManager(
+            final @NonNull ExecutionCoordinator<C> executionCoordinator
+    ) {
+        super(executionCoordinator, new MinestomRegistrationHandler<>());
+        ((MinestomRegistrationHandler<C>) this.commandRegistrationHandler()).initialize(this);
+        this.senderMapper = new SenderMapper<>() {
+            @NotNull
+            @Override
+            public C map(@NotNull CommandSender base) {
+                return (C) base;
+            }
+
+            @Override
+            public @NonNull CommandSender reverse(@NonNull C mapped) {
+                return (CommandSender) mapped;
+            }
+        };
+    }
+
+    public MinestomCommandManager(
             final @NonNull ExecutionCoordinator<C> executionCoordinator,
             final @NonNull SenderMapper<CommandSender, C> senderMapper
     ) {
         super(executionCoordinator, new MinestomRegistrationHandler<>());
+        ((MinestomRegistrationHandler<C>) this.commandRegistrationHandler()).initialize(this);
         this.senderMapper = senderMapper;
     }
 
--- cloudstom/src/main/java/net/minesprawl/cloudstom/MinestomRegistrationHandler.java
+++ cloudstom/src/main/java/net/minesprawl/cloudstom/MinestomRegistrationHandler.java
@@ -1,12 +1,39 @@
 package net.minesprawl.cloudstom;
 
+import net.minestom.server.MinecraftServer;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.incendo.cloud.Command;
+import org.incendo.cloud.component.CommandComponent;
+import org.incendo.cloud.context.CommandContext;
 import org.incendo.cloud.internal.CommandRegistrationHandler;
 
+import java.util.Collection;
+
 public class MinestomRegistrationHandler<C> implements CommandRegistrationHandler<C> {
+    private MinestomCommandManager<C> commandManager;
+
+    MinestomRegistrationHandler<C> initialize(MinestomCommandManager<C> commandManager) {
+        this.commandManager = commandManager;
+        return this;
+    }
+
     @Override
-    public boolean registerCommand(@NonNull Command<C> command) {
-        return false; // TODO
+    public boolean registerCommand(@NonNull Command<C> cloudCommand) {
+        CommandComponent<C> component = cloudCommand.rootComponent();
+        String name = component.name();
+        Collection<String> aliases = component.alternativeAliases();
+
+        net.minestom.server.command.builder.Command command = new net.minestom.server.command.builder.Command(
+                name,
+                aliases.toArray(new String[0])
+        );
+
+        this.commandManager.commandTree().getNamedNode(command.getName()).children().forEach(child -> {
+        });
+
+        command.setDefaultExecutor((sender, context) -> cloudCommand.commandExecutionHandler().execute(new CommandContext<>(commandManager.senderMapper().map(sender), commandManager)));
+
+        MinecraftServer.getCommandManager().register(command);
+        return true;
     }
 }
